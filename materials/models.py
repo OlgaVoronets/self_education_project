91defd42bc7f40ff3c49d928ecc7b10dfa040a65
olga voronets <voronets80@gmail.com>
2024-04-18 15:25:56 +0300

    Описаны модели учебных материалов, тестового задания и ответов к тестам

--- materials/models.py
+++ materials/models.py
@@ -5,21 +5,53 @@ NULLABLE = {'null': True, 'blank': True}
 
 class Course(models.Model):
     """Модель курса - тематический раздел обучения, например, Математика или Иностранный язык"""
-    pass
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
 
 
 class Lesson(models.Model):
     """Модель урока - теоретический материал, относится к одному конкретному курсу"""
-    pass
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
+    title = models.CharField(max_length=255, verbose_name='Название')
+    content = models.TextField(verbose_name='Материал урока')
+
+    def __str__(self):
+        return f'{self.title} из курса {self.course}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
 
 
-class Question(models.Model):
-    """Тестовое задание, относится к одному конкретному уроку, содержит задачу и
-    поле с правильным ответом"""
-    pass
+class Testing(models.Model):
+    """Тестовое задание, относится к одному конкретному уроку"""
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='Урок')
+    text = models.TextField(verbose_name='Текст задания')
+
+    def __str__(self):
+        return f'Тестовое задание к уроку {self.lesson}'
+
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
 
 
 class Answer(models.Model):
-    """Ответ пользователя на тестовое задание, относится к конкретному объекту Task,
-    присваивается пользователю"""
-    pass
+    """Ответ на тестовое задание, относится к конкретному объекту Testing"""
+    testing = models.ForeignKey(Testing, on_delete=models.CASCADE, verbose_name='Тестирование')
+    text = models.TextField(verbose_name='Текст ответа')
+    is_correct = models.BooleanField(default=False, verbose_name='Признак правильного ответа')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
