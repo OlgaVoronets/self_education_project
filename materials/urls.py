e045a99c94efc2b7614afd0cf1bed2808c8eeae3
olga voronets <voronets80@gmail.com>
2024-04-18 16:30:29 +0300

    Описаны urls модели урока

--- materials/urls.py
+++ materials/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from materials.apps import MaterialsConfig
-from materials.views import CourseCreateView, CourseListView, CourseDetailView, CourseUpdateView, CourseDeleteView
+from materials.views import CourseCreateView, CourseListView, CourseDetailView, CourseUpdateView, CourseDeleteView, \
+    LessonListView, LessonCreateView, LessonDetailView, LessonUpdateView, LessonDeleteView
 
 app_name = MaterialsConfig.name
 
@@ -12,13 +13,12 @@ urlpatterns = [
     path('course/<int:pk>/', CourseDetailView.as_view(), name='course_detail'),
     path('course/<int:pk>/update/', CourseUpdateView.as_view(), name='course_update'),
     path('course/<int:pk>/delete/', CourseDeleteView.as_view(), name='course_delete'),
-
     #  Модель урока
-    # path('lesson/', LessonListView.as_view(), name='lesson_list'),
-    # path('lesson/create/', LessonCreateView.as_view(), name='lesson_create'),
-    # path('lesson/<int:pk>/', LessonDetailView.as_view(), name='lesson_detail'),
-    # path('lesson/<int:pk>/update/', LessonUpdateView.as_view(), name='lesson_update'),
-    # path('lesson/<int:pk>/delete/', LessonDeleteView.as_view(), name='lesson_delete'),
-
+    path('lesson/', LessonListView.as_view(), name='lesson_list'),
+    path('lesson/create/', LessonCreateView.as_view(), name='lesson_create'),
+    path('lesson/<int:pk>/', LessonDetailView.as_view(), name='lesson_detail'),
+    path('lesson/<int:pk>/update/', LessonUpdateView.as_view(), name='lesson_update'),
+    path('lesson/<int:pk>/delete/', LessonDeleteView.as_view(), name='lesson_delete'),
+    #  Модель тестирования
 
 ]
